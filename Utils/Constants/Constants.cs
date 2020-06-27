fad08a9f3961d8ed54d28ae30accfb05ec981ce6
lhthang1998 <lhthang.1998@gmail.com>
2020-06-27 10:48:06 +0700

    get username from token

--- Utils/Constants/Constants.cs
+++ Utils/Constants/Constants.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +12,13 @@ namespace cinema_core.Utils.Constants
     {
         public const string SHOWTIME_STATUS_ACTIVE = "OPEN";
         public const string SHOWTIME_STATUS_INACTIVE = "CLOSED";
+
+        public static string GetUsername(HttpRequest request)
+        {
+            string token = request.Headers[HeaderNames.Authorization];
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token.Split("Bearer ")[1]);
+            var username = jwt.Claims.First(claim => claim.Type == "sub").Value;
+            return username;
+        }
     }
 }
